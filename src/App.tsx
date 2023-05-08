eb6acd24054602b70da8b34dc593ffa79503eb39
Keerty Verma <keertyverma1303@gmail.com>
2023-05-08 10:13:08 +0530

    Sharing state using React context

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useReducer } from "react";
 import "./App.css";
-import PostList from "./react-query/PostList";
-import TodoForm from "./react-query/TodoForm";
-import TodoList from "./react-query/TodoList";
-import Counter from "./state-management/Counter";
-import TaskList from "./state-management/TaskList";
+import HomePage from "./state-management/HomePage";
+import NavBar from "./state-management/NavBar";
+import TaskContext from "./state-management/context/taskContext";
+import taskReducer from "./state-management/reducers/taskReducer";
 
 function App() {
-  return <TaskList />;
+  //1. lift step up in closed parent
+  const [tasks, dispatch] = useReducer(taskReducer, []);
+
+  // 3. Wrap component tree with context provider and share data
+  return (
+    <>
+      <TaskContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <HomePage />
+      </TaskContext.Provider>
+    </>
+  );
 }
 
 export default App;

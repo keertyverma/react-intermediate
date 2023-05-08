06af72bb16c637204b98073ff897aa09adc9767c
Keerty Verma <keertyverma1303@gmail.com>
2023-05-08 10:53:00 +0530

    Create custom provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import { useReducer } from "react";
 import "./App.css";
+import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
 import TaskContext from "./state-management/context/taskContext";
 import taskReducer from "./state-management/reducers/taskReducer";
-import authReducer from "./state-management/reducers/authReducer";
-import AuthContext from "./state-management/context/authContext";
 
 function App() {
   //1. lift step up in closed parent
   const [tasks, taskDispatch] = useReducer(taskReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, "");
 
   // 3. Wrap component tree with context provider and share data
   return (
-    <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+    <AuthProvider>
       <TaskContext.Provider value={{ tasks, dispatch: taskDispatch }}>
         <NavBar />
         <HomePage />
       </TaskContext.Provider>
-    </AuthContext.Provider>
+    </AuthProvider>
   );
 }
 
